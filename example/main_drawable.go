0ec4807d2987ba3af8de56841088f77ce0489b11
Mourey jerome <jerome@everycheck.fr>
2020-07-14 11:35:12 +0200

    split drawable and no drawable example

--- example/main_drawable.go
+++ example/main_drawable.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/canadadry/drawable/geometry"
-	"github.com/canadadry/drawable/program"
-	"github.com/canadadry/drawable/program/shader"
-	"github.com/canadadry/drawable/texture"
+	"github.com/canadadry/drawable"
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
 	"github.com/go-gl/mathgl/mgl32"
@@ -15,12 +12,6 @@ import (
 
 const windowWidth = 800
 const windowHeight = 600
-
-var (
-	projection = mgl32.Perspective(mgl32.DegToRad(45.0), float32(windowWidth)/windowHeight, 0.1, 10.0)
-	camera     = mgl32.LookAtV(mgl32.Vec3{3, 3, 3}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0})
-	model      = mgl32.Ident4()
-)
 
 func init() {
 	runtime.LockOSThread()
@@ -50,38 +41,20 @@ func main() {
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	fmt.Println("OpenGL version", version)
 
-	p, err := program.New(shader.Basic)
+	cube := drawable.Cube{
+		Param: drawable.CubeParam{
+			Size:       1,
+			Texture:    "square.png",
+			Projection: mgl32.Perspective(mgl32.DegToRad(45.0), float32(windowWidth)/windowHeight, 0.1, 10.0),
+			Camera:     mgl32.LookAtV(mgl32.Vec3{3, 3, 3}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0}),
+			Model:      mgl32.Ident4(),
+		},
+	}
+
+	err = cube.Prepare()
 	if err != nil {
 		panic(err)
 	}
-	p.Use()
-
-	err = p.Uniform(shader.Basic.Uniform[0], projection)
-	if err != nil {
-		panic(err)
-	}
-	err = p.Uniform(shader.Basic.Uniform[1], camera)
-	if err != nil {
-		panic(err)
-	}
-	err = p.Uniform(shader.Basic.Uniform[2], model)
-	if err != nil {
-		panic(err)
-	}
-
-	t, err := texture.FromImage("square.png")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	part := []program.VBOPart{
-		{shader.Basic.Attribute[0], 3},
-		{shader.Basic.Attribute[1], 2},
-	}
-
-	g := geometry.NewCube(mgl32.Vec3{}, 1.0)
-
-	b := program.NewBuffer(p, part, g.Buf)
 
 	gl.Enable(gl.DEPTH_TEST)
 	gl.DepthFunc(gl.LESS)
@@ -90,13 +63,8 @@ func main() {
 	for !window.ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-		model = mgl32.HomogRotate3D(float32(glfw.GetTime()), mgl32.Vec3{0, 1, 0})
-
-		p.Use()
-		p.Uniform(shader.Basic.Uniform[2], model)
-		b.Bind()
-		t.Bind()
-		g.Draw()
+		cube.Param.Model = mgl32.HomogRotate3D(float32(glfw.GetTime()), mgl32.Vec3{0, 1, 0})
+		cube.Draw()
 
 		window.SwapBuffers()
 		glfw.PollEvents()
--- readme.md
+++ readme.md
@@ -124,6 +124,84 @@ Instead of using a hard to read and maintain OpenGL interface you can use more s
 
 ## Futur
 
+For now with drawable this look like this : 
+
+```go 
+package main
+
+import (
+	"fmt"
+	"github.com/canadadry/drawable"
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.2/glfw"
+	"github.com/go-gl/mathgl/mgl32"
+	"log"
+	"runtime"
+)
+
+const windowWidth = 800
+const windowHeight = 600
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func main() {
+	if err := glfw.Init(); err != nil {
+		log.Fatalln("failed to initialize glfw:", err)
+	}
+	defer glfw.Terminate()
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+	window, err := glfw.CreateWindow(windowWidth, windowHeight, "Cube", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	fmt.Println("OpenGL version", version)
+
+	cube := drawable.Cube{
+		Param: drawable.CubeParam{
+			Size:       1,
+			Texture:    "square.png",
+			Projection: mgl32.Perspective(mgl32.DegToRad(45.0), float32(windowWidth)/windowHeight, 0.1, 10.0),
+			Camera:     mgl32.LookAtV(mgl32.Vec3{3, 3, 3}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0}),
+			Model:      mgl32.Ident4(),
+		},
+	}
+
+	err = cube.Prepare()
+	if err != nil {
+		panic(err)
+	}
+
+	gl.Enable(gl.DEPTH_TEST)
+	gl.DepthFunc(gl.LESS)
+	gl.ClearColor(1.0, 1.0, 1.0, 1.0)
+
+	for !window.ShouldClose() {
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+		cube.Param.Model = mgl32.HomogRotate3D(float32(glfw.GetTime()), mgl32.Vec3{0, 1, 0})
+		cube.Draw()
+
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
+```
+It does the same thing as the previous example but you can only draw a cube... 
+
 I want to build a more complet set of tool on top of those 3 modules (`geometry`,`program`,`texture`) 
 My inspiration will came for what SFML has done in its Graphics Module.
 
